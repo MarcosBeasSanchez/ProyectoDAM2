4af686f831db79979ecf530f5045d089f896873f
MarcosBeasSanchez <mbs441@educa.madrid.org>
2024-10-10 11:26:44 +0200

    perfil dev2 creado y /login funcionando y /register no

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from  'express'  // importamos morgan
-//import morgan from 'morgan' //  informa de las solicitudes de entrada
-//import routes from './routes/Auth.routes.js'
+import morgan from 'morgan' //  informa de las solicitudes de entrada
+import routes from './routes//Auth.routes.js'
 
 
 const app = express() // para que use morgan con el .use
 //app.use(express.json());
-//app.use(morgan());
-//app.use(routes)
+app.use(morgan('dev'));
+app.use(routes)
 
 export default app;
