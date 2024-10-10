54e1dccd6794ecbe099cd164f3bb3460c2e8b71e
MarcosBeasSanchez <mbs441@educa.madrid.org>
2024-10-10 13:07:12 +0200

    todo funciona perfectamente menos register

--- src/controllers/Auth.controllers.js
+++ src/controllers/Auth.controllers.js
@@ -1,11 +1,15 @@
 //Enviamos un res a nuestro front mediante una funcion flecha
-import userModel from "../models/user.model.js";
+
+//import userModel from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
-export const register = (req, res) => {
+export const register = async (req, res) => {
+
     const { username, email, password } = req.body
-    const passwordHash = bcrypt.hash(password, 10)
-    console.log((passwordHash).toString())
+
+    //const passwordHash = bcrypt.hash(password, 10)
+
+    //console.log((passwordHash).toString())
 
     try {
         const newUser = new User({
@@ -14,13 +18,22 @@ export const register = (req, res) => {
             password: passwordHash
         })
 
-        const UserSchema = newUser.save()
-        res.json(UsernameSalvado)
+        const userSaved = await newUser.save()
+
+        res.json(userSaved)
+
+        /*res.json({
+            _id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email
+        })*/
 
     } catch (error) {
         console.log(error)
     }
+
 }
+
 
 export const login = (req, res) => {
     res.send("Logeando al usuario") // Login  la respuesta del back
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,23 +1,23 @@
 import mongoose from "mongoose";
 
 const authSchema = new mongoose.Schema({
-    username:{
-        type:String,
+    username: {
+        type: String,
         required: true,
-        trim:true
+        trim: true
     },
-    email:{
-        type:String,
+    email: {
+        type: String,
         required: true,
-        trim:true,
-        unique:true
+        trim: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     }
-}
-//{timestamps:true}
+},
+    { timestamps: true }
 );
 
-export default mongoose.model('User',authSchema);
+export default mongoose.model('User', authSchema);
